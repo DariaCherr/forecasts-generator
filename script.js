8899e586e2c0739a9e8eda7ae839fe64f8fe85db
DariaCherr <DaskhenCher@mail.ru>
2022-11-28 20:07:23 +0300

    Домашнее задание 5 правки

--- script.js
+++ script.js
@@ -1,3 +1,57 @@
+const button = document.querySelector('.forecast-btn');
+const forecastTemplate = document.querySelector('#forecast-item');
+const forecastContainer = document.querySelector('.forecasts');
+const forecastTitle = document.querySelector('.current-forecast h1');
+const forecastChance = document.querySelector('.current-forecast p');
+
+
+const firstForecast = 'Найдешь хорошую работу';
+const secondForecast = 'Будет мир во всем мире';
+const thirdForecast = 'Хорошие новости';
+const fourthForecast = 'Все будет хорошо';
+const fifthForecast = 'Грядут перемены к лучшему';
+
+function generateRandomValue(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function addForecast() {
+    const newForecast = forecastTemplate.content;
+    newForecast.querySelector('h3').textContent = forecastTitle.textContent;
+    newForecast.querySelector('p').textContent = forecastChance.textContent;
+    return newForecast;
+}
+
+button.addEventListener('click', function() {
+    if (forecastTitle.textContent && forecastChance.textContent) {
+        const newForecasts = addForecast();
+        forecastContainer.prepend(newForecasts);
+    }
+
+    let random = generateRandomValue(0, 5);
+
+    switch (random) {
+        case 0:
+            forecastTitle.textContent = firstForecast
+            break;
+        case 1:
+            forecastTitle.textContent = secondForecast
+            break;
+        case 2:
+            forecastTitle.textContent = thirdForecast
+            break;
+        case 3:
+            forecastTitle.textContent = fourthForecast
+            break;
+        case 4:
+            forecastTitle.textContent = fifthForecast
+            break;
+    }
+
+    let amount = generateRandomValue(0, 100);
+    forecastChance.textContent = 'Вероятность: ' + amount + '%';
+})
+
 /* Генерация предсказания должна происходить при клике на кнопку «предсказать судьбу» */
 
 /* Заранее заготовь 3-5 предсказаний и в зависимости от того, как лягут карты судьбы (или что скажет Math.random) показывай их пользователю */
